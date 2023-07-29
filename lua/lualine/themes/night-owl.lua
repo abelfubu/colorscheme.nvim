3b889e09282af0b3aa6c0a06e01aca025da34be2
Abel de la Fuente <abelfubu@gmail.com>
2023-07-29 09:58:58 +0200

    feat(chore): lualine

--- lua/lualine/themes/night-owl.lua
+++ lua/lualine/themes/night-owl.lua
@@ -1,6 +1,6 @@
 local colors = {
 	dark = "#1e222a",
-	light = "#d3c6aa",
+	light = "#83c092",
 	magenta = "#e67e80",
 	green = "#a7c080",
 	yellow = "#dbbc7f",
@@ -14,38 +14,32 @@ local night_owl = {
 	normal = {
 		a = { bg = colors.magenta, fg = colors.dark, gui = "bold" },
 		b = { bg = "NONE", fg = colors.green },
-		c = { bg = "NONE", fg = colors.light },
-		d = { bg = "NONE", fg = colors.blue },
+		c = { bg = "NONE", fg = colors.yellow },
 	},
 	insert = {
 		a = { bg = colors.green, fg = colors.dark, gui = "bold" },
 		b = { bg = "NONE", fg = colors.green },
-		c = { bg = "NONE", fg = colors.light },
-		d = { bg = "NONE", fg = colors.blue },
+		c = { bg = "NONE", fg = colors.yellow },
 	},
 	visual = {
 		a = { bg = colors.yellow, fg = colors.dark, gui = "bold" },
 		b = { bg = "NONE", fg = colors.green },
-		c = { bg = "NONE", fg = colors.light },
-		d = { bg = "NONE", fg = colors.blue },
+		c = { bg = "NONE", fg = colors.yellow },
 	},
 	replace = {
 		a = { bg = colors.orange, fg = colors.dark, gui = "bold" },
 		b = { bg = "NONE", fg = colors.green },
-		c = { bg = "NONE", fg = colors.light },
-		d = { bg = "NONE", fg = colors.blue },
+		c = { bg = "NONE", fg = colors.yellow },
 	},
 	command = {
 		a = { bg = colors.cyan, fg = colors.dark, gui = "bold" },
 		b = { bg = "NONE", fg = colors.green },
-		c = { bg = "NONE", fg = colors.light },
-		d = { bg = "NONE", fg = colors.blue },
+		c = { bg = "NONE", fg = colors.yellow },
 	},
 	inactive = {
 		a = { bg = colors.dark_blue, fg = colors.light_blue, gui = "bold" },
 		b = { bg = "NONE", fg = colors.green },
-		c = { bg = "NONE", fg = colors.light },
-		d = { bg = "NONE", fg = colors.blue },
+		c = { bg = "NONE", fg = colors.yellow },
 	},
 }
 
