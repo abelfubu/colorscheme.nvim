9fd8cbcb93121258811ce97443a1b824160afe3b
Abel de la Fuente <abelfubu@gmail.com>
2023-07-30 16:02:34 +0200

    feat(chore): end of buffer

--- lua/everforest/init.lua
+++ lua/everforest/init.lua
@@ -11,7 +11,7 @@ M.setup = function()
 
 	vim.o.termguicolors = true
 	vim.g.colors_name = "everforest"
-	vim.opt.fillchars = { eob = " " }
+	-- vim.opt.fillchars = { eob = " " }
 
 	theme.set_highlights()
 end
