25cd05c0a2b4c98144dca816e94a35c63159f13b
Abel de la Fuente <abelfubu@gmail.com>
2023-07-30 16:00:16 +0200

    feat(chore): navbuddy

--- lua/everforest/theme.lua
+++ lua/everforest/theme.lua
@@ -62,7 +62,6 @@ theme.set_highlights = function()
 	hl(0, "TabLineSel", { fg = c.gray2, bg = c.tab_active_bg })
 	hl(0, "TabLine", { fg = c.ui_border, bg = c.tab_inactive_bg })
 	hl(0, "TabLineFill", { fg = c.bg, bg = "NONE" })
-	hl(0, "EndOfBuffer", { fg = "NONE", bg = "NONE" })
 
 	-- Treesitter
 	hl(0, "@class.constructor", { fg = c.blue, bg = "NONE", italic = true })
