94db96d08c344ed0fe1df35785c898641ca681c0
Abel de la Fuente <abelfubu@gmail.com>
2023-08-01 14:56:46 +0200

    feat(chore): lualine

--- lua/lualine/themes/everforest.lua
+++ lua/lualine/themes/everforest.lua
@@ -15,7 +15,6 @@ local night_owl = {
 		a = { bg = colors.magenta, fg = colors.dark, gui = "bold" },
 		b = { bg = "NONE", fg = colors.green },
 		c = { bg = "NONE", fg = colors.yellow },
-		z = { bg = colors.light_blue, fg = colors.dark },
 	},
 	insert = {
 		a = { bg = colors.green, fg = colors.dark, gui = "bold" },
